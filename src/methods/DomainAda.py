0aac5e255beafeabdd26432e0ed87f68923bda85
Diagle <189926112@qq.com>
2026-01-08 19:21:51 +0800

    add deeplearn basis

--- src/methods/DomainAda.py
+++ src/methods/DomainAda.py
@@ -1,5 +1,8 @@
+from typing import Self
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
+from ot.da import *
+
 
 class CORAL(BaseEstimator, TransformerMixin):
     """
@@ -19,6 +22,7 @@ class CORAL(BaseEstimator, TransformerMixin):
     """
 
     def __init__(self, regularization=1.0):
+        super().__init__()
         self.regularization = regularization
 
     def _matrix_power(self, M, power):
@@ -37,7 +41,7 @@ class CORAL(BaseEstimator, TransformerMixin):
         Xt : target data, shape (n_samples_t, n_features)
         """
         Xs = X[sample_domain == 1]
-        Xt = X[sample_domain == 0]
+        Xt = X[sample_domain == -1]
         
         # 协方差矩阵
         Cs = np.cov(Xs, rowvar=False) + self.regularization * np.eye(Xs.shape[1])
@@ -69,22 +73,105 @@ class CORAL(BaseEstimator, TransformerMixin):
     def fit_transform(self, X, sample_domain):
         """Fit and then transform the source data."""
         Xs = X[sample_domain == 1]
-        Xt = X[sample_domain == 0]
+        Xt = X[sample_domain == -1]
         return self.fit(X, sample_domain).transform(Xs)
 
 class TCA(BaseEstimator):
     pass
 
-class OT(BaseEstimator):
+## 封装ot.da最优运输方法
+# OT原始形式最优运输域适应
+class OT_Exact(BaseEstimator):
     
     def __init__(self):
-        super.__init__()
+        super().__init__()
+        self.emd_transport = EMDTransport()
 
-    def fit(self):
-        pass
+    def fit(self, X, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        self.emd_transport.fit(Xs=Xs, Xt=Xt)
+        return self
     
-    def transform(self):
-        pass
+    def transform(self, Xs):
+        return self.emd_transport.transform(Xs)
     
-    def fit_transform(self):
-        return self.fit().transform()
+    def fit_transform(self,X,sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        return self.fit(X, sample_domain).transform(Xs)
+# OT信息熵形式最优运输域适应    
+class OT_IT(BaseEstimator):
+
+    def __init__(self, reg_alpha=1):
+        super().__init__()
+        self.sinkhorn_transport = SinkhornTransport(reg_e=reg_alpha)
+    
+    def fit(self, X, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        return self.sinkhorn_transport.fit(Xs=Xs, Xt=Xt)
+    
+    def transform(self, Xs):
+        return self.sinkhorn_transport.transform(Xs)
+    
+    def fit_transform(self, X, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        return self.fit(X, sample_domain).transform(Xs)
+
+# OT Group Lasso
+class OT_GL(BaseEstimator):
+    def __init__(self, reg_e=1e-1,reg_cl=1e0):
+        super().__init__()
+        self.sinkhornLpl1_transport = SinkhornLpl1Transport(reg_e=reg_e, reg_cl=reg_cl)
+
+    def fit(self, X, y, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        ys = y[sample_domain == 1]
+        return self.sinkhornLpl1_transport.fit(Xs=Xs, ys=ys, Xt=Xt)
+    
+    def transform(self, Xs):
+        return self.sinkhornLpl1_transport.transform(Xs)
+    
+    def fit_transform(self, X, y, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        return self.fit(X, y, sample_domain).transform(Xs)
+
+# OT Laplace
+class OT_Laplace():
+    def __init__(self, reg_lap=1, reg_src = 1):
+        super().__init__()
+        self.emdLaplace_transport = EMDLaplaceTransport(reg_lap=reg_lap, reg_src=reg_src)
+    
+    def fit(self, X, y, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        ys = y[sample_domain == 1]
+        return self.emdLaplace_transport.fit(Xs=Xs, ys=ys, Xt=Xt)
+    
+    def transform(self, Xs):
+        return self.emdLaplace_transport.transform(Xs)
+    
+    def fit_transform(self, X, y, sample_domain):
+        Xs = X[sample_domain == 1]
+        return self.fit(X, y, sample_domain).transform(Xs)
+
+class OT_Unbalance(BaseEstimator):
+    def __init__(self,reg_e=1, reg_m=0.1):
+        super().__init__()
+        self.unbalancedSinkhorn_transport = UnbalancedSinkhornTransport(reg_e=reg_e, reg_m=reg_m)
+
+    def fit(self, X, sample_domain):
+        Xs = X[sample_domain == 1]
+        Xt = X[sample_domain == -1]
+        return self.unbalancedSinkhorn_transport.fit(Xs)
+
+    def transform(self, Xs):
+        return self.unbalancedSinkhorn_transport.transform(Xs)
+        
+    def fit_transform(self, X, sample_domain):
+        Xs = X[sample_domain == 1]
+        return self.unbalancedSinkhorn_transport.fit(X, sample_domain).transform(Xs)
--- src/utils/PLOT.py
+++ src/utils/PLOT.py
@@ -5,53 +5,77 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 
 
-def PCA_plot(X,y,sample_domain,component_order=1,label_names=[0,1]):
-
+def Domain_plot(X, y ,sample_domain):
     src_idx = np.where(sample_domain == 1)
-    tar_idx = np.where(sample_domain == 0)
+    tar_idx = np.where(sample_domain == -1)
     Xs = X[src_idx]
     Xt = X[tar_idx]
-    y_ = (y - label_names[0]) /(label_names[1] - label_names[0])
+    ys = y[src_idx]
+    yt = y[tar_idx]
 
-    ys = y_[src_idx]
-    yt = y_[tar_idx]
+    plt.subplots(1,2,figsize=(10,6))
+    plt.subplot(121)
+    plt.scatter(Xs[:, 0], Xs[:, 1], c=ys, marker="+", label="Source samples", alpha=0.3, s=10)
+    plt.xticks([])
+    plt.yticks([])
+    plt.legend(loc=0)
+    plt.title("Source  samples")
+    
+    plt.subplot(122)
+    plt.scatter(Xt[:, 0], Xt[:, 1], c=yt, marker="o", label="Target samples", alpha=0.3, s=10)
+    plt.xticks([])
+    plt.yticks([])
+    plt.legend(loc=0)
+    plt.title("Target samples")
+    plt.tight_layout()
 
-    cls = ['g','y','grey','grey']
-    labels_s = [cls[int(i)] for i in ys]
-    labels_t = [cls[int(i)+2] for i in yt]
-    n_points1 = np.sum(y_==0)
-    n_points2 = np.sum(y_==1)
+def PCA_plot(X, y ,sample_domain):
+
+    src_idx = np.where(sample_domain == 1)
+    tar_idx = np.where(sample_domain == -1)
+    Xs = X[src_idx]
+    Xt = X[tar_idx]
+    ys = y[src_idx]
+    yt = y[tar_idx]
+
 
     pca = PCA(svd_solver='full',n_components=2)
     pca.fit(X)
     Us = pca.transform(Xs)
     Ut = pca.transform(Xt)
-    u = np.concatenate((Us,Ut),axis=0)
-    
-    nbins = 30
 
     plt.subplots(1,2,figsize=(10,6))
     plt.subplot(121)
-    plt.scatter(Us[:,0],Us[:,1],c=labels_s,marker='*', label='Source', alpha=0.3,s=10)
-    plt.scatter(Ut[:,0],Ut[:,1],c=labels_t,marker='d', label='Target', alpha=0.3,s=10)
+    plt.scatter(Us[:,0], Us[:,1], c=ys, cmap='tab10', marker='+', label='Source', alpha=0.3, s=10)
     plt.legend()
     plt.xlabel('PC1',fontsize=20)
     plt.ylabel('PC2',fontsize=20)
     plt.axis('equal')
-
+    plt.title("Source  samples")
+    
     plt.subplot(122)
-    rng = (np.min(np.min(u[:,component_order-1])),np.max(np.max(u[:,component_order-1])))
-    plt.hist(Us[0:n_points1,component_order-1],bins=nbins,color='g',alpha=0.3,range=rng)
-    plt.hist(Us[n_points1:n_points1+n_points2,component_order-1],bins=nbins,color='y',alpha=0.3,range=rng)
-    plt.hist(Ut[:,component_order-1],bins=nbins,color='grey',alpha=0.3,range=rng)
-    plt.title(f'PC{component_order}',fontsize=20)
+    plt.scatter(Ut[:,0], Ut[:,1], c=yt, cmap='tab10', marker='o', label='Target', alpha=0.3, s=10)
+    plt.legend()
+    plt.xlabel('PC1',fontsize=20)
+    plt.ylabel('PC2',fontsize=20)
+    plt.axis('equal')
+    plt.title("Target  samples")
+    plt.tight_layout()
+
+def curve_plot(x, y1, y2, label_list):
+    plt.figure(figsize=(8,5))
+    plt.plot(x, y1, label=label_list[0])
+    plt.plot(x, y2 , 'm--', label=label_list[1])
+    plt.grid()
+    plt.legend()
     plt.show()
+
 
 if __name__ ==  '__main__':
     Xs = np.concatenate((np.random.normal(2,10,(50,1000)) , np.random.normal(-2,10,(50,1000))),axis=0)
     Xt = (Xs[np.r_[0:40,50:90]] **2)/50
-    y = np.array([0]*50 + [1]*50 + [2]*40 + [3]*40)
+    y = np.array([0]*50 + [1]*50 + [0]*40 + [1]*40)
     X = np.concatenate((Xs,Xt),axis=0)
-    sample_domain = np.array([1]*100+[0]*80)
+    sample_domain = np.array([1]*100+[-1]*80)
 
     PCA_plot(X,y,sample_domain)
